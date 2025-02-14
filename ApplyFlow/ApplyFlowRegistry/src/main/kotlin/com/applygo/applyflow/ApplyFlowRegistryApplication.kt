9057d89f4e79a13b5d251a462b9aa8f5721fdb83
Paraglider <158726282+TechParaglide@users.noreply.github.com>
2025-02-15 03:55:22 +0530

    ADMIN -U [0.0.1]

---
+++ ApplyFlow/ApplyFlowRegistry/src/main/kotlin/com/applygo/applyflow/ApplyFlowRegistryApplication.kt
@@ -0,0 +1,12 @@
+package com.applygo.applyflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApplyFlowRegistryApplication
+
+fun main(args: Array<String>) {
+    runApplication<ApplyFlowRegistryApplication>(*args)
+    println("Contributor: @Parth-Thakor")
+}

f1c53f02e1603d8b315a266f9fea3bd0782694c8
Paraglider <158726282+TechParaglide@users.noreply.github.com>
2025-02-18 15:46:47 +0530

    PARTH -b [0.1.5][Docker Containerization]

--- ApplyFlow/ApplyFlowConfig/src/main/kotlin/com/applygo/applyflow/ApplyFlowConfigApplication.kt
+++ ApplyFlow/ApplyFlowConfig/src/main/kotlin/com/applygo/applyflow/ApplyFlowConfigApplication.kt
@@ -12,13 +12,11 @@
 
 package com.applygo.applyflow
 
-import com.applygo.applyflow.services.EnableDotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
 
-@EnableDotenv
 @EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
